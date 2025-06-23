60d5736783272969e5fa59404e7d611f09ce76bd
KakDeLa <scpw35533@gmail.com>
2025-06-23 17:04:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const chatBox = document.getElementById("chat-box");
-const msgInput = document.getElementById("msgInput");
+window.addEventListener("load", () => {
+  document.getElementById("preloader").style.display = "none";
+  document.getElementById("main-content").classList.remove("hidden");
+});
 
+// Чат-функция (локальная)
 function sendMsg() {
+  const chatBox = document.getElementById("chat-box");
+  const msgInput = document.getElementById("msgInput");
   const msg = msgInput.value.trim();
   if (msg) {
     const msgElem = document.createElement("div");
