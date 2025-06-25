3a1549898c3de35c08738c40ed28f34ce4a953fd
KakDeLa <scpw35533@gmail.com>
2025-06-25 16:41:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 window.addEventListener("load", () => {
-  // Убираем прелоадер
   document.getElementById("preloader").style.display = "none";
-  // Показываем основной контент с анимацией
   document.getElementById("main-content").classList.remove("hidden");
 });
-
-function sendMsg() {
-  const chatBox = document.getElementById("chat-box");
-  const msgInput = document.getElementById("msgInput");
-  const msg = msgInput.value.trim();
-  if (!msg) return;
-  const msgElem = document.createElement("div");
-  msgElem.textContent = "👤 Гость: " + msg;
-  chatBox.appendChild(msgElem);
-  chatBox.scrollTop = chatBox.scrollHeight;
-  msgInput.value = "";
-}
